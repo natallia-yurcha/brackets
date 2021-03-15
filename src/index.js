6fd26b7414b8b51a1f8b9245e3bda030303f2170
Natallia Yurcha <natallia.yurcha@gmail.com>
2021-03-15 22:53:42 +0300

    feat: implement bracket function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
+function isOpenBracket(str, bracketsConfig) {
+  for (bracketPair of bracketsConfig) {
+    if (bracketPair[0] === str) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function isOpentBracketEqualToClosed(openBracket, bracketsConfig) {
+  for (bracketPair of bracketsConfig) {
+    if (bracketPair[0] === openBracket && bracketPair[1] === openBracket) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function getClosedBracketFor(openBracket, bracketsConfig) {
+  for (bracketPair of bracketsConfig) {
+    if (bracketPair[0] === openBracket) {
+      return bracketPair[1];
+    }
+  }
+}
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBrackets = [];
+  for (let i = 0; i < str.length; i++) {
+    let currentElementStr = str[i];
+
+    if (isOpenBracket(currentElementStr, bracketsConfig)) {
+      if (isOpentBracketEqualToClosed(currentElementStr, bracketsConfig)) {
+        if (openBrackets[openBrackets.length - 1] === currentElementStr) {
+          openBrackets.pop();
+        } else {
+          openBrackets.push(currentElementStr);
+        }
+      } else {
+        openBrackets.push(currentElementStr);
+      }
+    } else {
+      let lastOpenBracket = openBrackets.pop();
+      if (getClosedBracketFor(lastOpenBracket, bracketsConfig) !== currentElementStr) {
+        return false;
+      }
+    }
+  }
+
+  return openBrackets.length === 0
 }
